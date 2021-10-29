aba8e614937db8aeb82cb7784ad8b9653b233848
abhyuday2003 <92596193+abhyuday2003@users.noreply.github.com>
2021-10-29 22:59:26 +0530

    Update 2d_array_new.cpp

--- 2d_array_new.cpp
+++ 2d_array_new.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-    cout<<"Enter the number of rows and columns: ";
-    int r,c,i,j;
-    cin>>r>>c;
-    int arr[r][c];
-    cout<<"The elements along the row: ";
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++)
+int main()
+{
+    cout<<"\nEnter the number of rows and columns: ";
+    int row,col,i,j;
+    cin>>row>>col;
+    int arr[row][col];
+    cout<<"\n Enter the elements along the row:";
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++)
         cin>>arr[i][j];
-        cout<<endl;
     }
     cout<<"The matrix is: "<<endl;
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++)
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++)
         cout<<arr[i][j]<<" ";
         cout<<endl;
     }
+    return 0;
 }
